72970c3ec78f85e156602700e0605d786dacd606
George Farcasiu <farcasiu.george@gmail.com>
2018-04-01 21:56:42 -0500

    Added basic signalling support

--- index.js
+++ index.js
@@ -1,6 +1,18 @@
 const EventEmitter = require('events');
 const WebSocket = require('ws');
 
+const util = require('util')
+
+/***********/
+/* PRIVATE */
+/***********/
+
+const MessageType = {
+    ERROR: 'ERROR',
+    SIGNAL: 'SIGNAL',
+    SIGN_UP: 'SIGN_UP',
+}
+
 /**********/
 /* PUBLIC */
 /**********/
@@ -11,11 +23,17 @@ class Server extends EventEmitter {
 
         let self = this
 
+        // TODO : don't store this in memory...
+        self._idToClient = {};
+
+        // Initialize websocket server
         self._wss = new WebSocket.Server({
             clientTracking: true,
             port: 8080,
         })
 
+        // TODO : update this to broadcast to friends only
+        // TODO : make a member function of `Server` of websocket server
         self._wss.broadcast = (data) => {
           self._wss.clients.forEach((client) => {
             if (client.readyState === WebSocket.OPEN) {
@@ -24,30 +42,107 @@ class Server extends EventEmitter {
           })
         }
 
+        // Triage messages to designated event handlers
         self._wss.on('connection', (ws) => {
             ws.on('message', (data) => {
                 data = JSON.parse(data)
-                self.emit(data.type, data.id, data.payload)
+
+                if (!Object.keys(MessageType).map((k) => MessageType[k]).includes(data.type)) {
+                    self._sendError(ws, util.format("The message type `%s` is not supported", data.type))
+                    return
+                }
+
+                self.emit(data.type, ws, data.id, data.payload)
             })
         })
 
-        self.on('signal', self.signal);
+        // Register handlers for every message type
+        self.on(MessageType.SIGNAL, self.handleSignal);
+        self.on(MessageType.SIGN_UP, self.handleSignUp);
     }
 
-    signal(clientId, payload) {
-        console.log('Client (' + clientId + ') sent signal for: ' + payload.peerId)
+    /***********/
+    /* PRIVATE */
+    /***********/
+
+    _sendError(ws, errorMsg) {
+        ws.send(JSON.stringify({
+            'type' : MessageType.ERROR,
+            'payload' : {
+                'message' : errorMsg,
+            }
+        }))
     }
 
-    // TODO : add register identifier handler 
+    /************/
+    /* HANDLERS */
+    /************/
+
+    /**
+     * Initiate signaling with another client
+     *
+     * @param {WebSocket} ws
+     * @param {String} clientId
+     * @param {Object} payload
+     */
+    handleSignal(ws, clientId, payload) {
+        if (!this._idToClient[payload.toId]) {
+            this._sendError(ws, util.format('Client `%s` is not online', payload.toId))
+            return
+        }
+
+        this._idToClient[payload.toId].send(JSON.stringify({
+            'type': MessageType.SIGNAL,
+            'payload' : payload,
+        }))
+    }
+
+    /**
+     * Sign up for the service by registering a newly generated public key
+     *
+     * @param {WebSocket} ws
+     * @param {String} clientId
+     * @param {Object} payload
+     */
+    handleSignUp(ws, _, payload) {
+        if (this._idToClient[payload.id]) {
+            this._sendError(ws, util.format('A client is already registered with ID: %s', payload.id))
+            return
+        }
+
+        this._idToClient[payload.id] = ws;
+    }
+
+    // TODO : notify clients when new clients have connected
 }
 
-class Client {
+class Client extends EventEmitter {
+    /**
+     * @param {String} id - The public key of the client being created
+     */
     constructor(id) {
+        super()
+
         let self = this
 
         self.id = id
 
         self._ws = new WebSocket('ws://localhost::8080');
+
+        // Triage messages to designated event handlers
+        self._ws.on('message', (data) => {
+            data = JSON.parse(data)
+
+            if (!Object.keys(MessageType).map((k) => MessageType[k]).includes(data.type)) {
+                self.emit(MessageType.ERROR, util.format('Received an unsupported message type from the server: %s', data.type))
+                return
+            }
+
+            self.emit(data.type, data.payload)
+        })
+
+        self.on(MessageType.ERROR, (payload) => { console.log(util.format('SERVER ERROR: %s', payload.message)) })
+        self.on(MessageType.SIGNAL, (payload) => { console.log(util.format('Received signal from `%s`', payload.fromId)) })
     }
 
     _queueSend(type, payload, cb) {
@@ -71,10 +166,21 @@ class Client {
         }
     }
 
-    signal(peerId, cb) {
+    signal(toId, cb) {
         this._queueSend(
-            'signal',
-            { 'peerId': peerId },
+            MessageType.SIGNAL,
+            {
+                'fromId': this.id,
+                'toId': toId,
+            },
+            cb
+        )
+    }
+
+    signUp(cb) {
+        this._queueSend(
+            MessageType.SIGN_UP,
+            { 'id': this.id },
             cb
         )
     }
